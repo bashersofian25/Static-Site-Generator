f5578cd9c06d68a128ff5c35ae9d7c985272d8ed
bashersofian25 <bsofian4@gmail.com>
2025-03-25 00:25:07 +0300

    Split node with delimiter function

--- src/test_util.py
+++ src/test_util.py
@@ -2,7 +2,7 @@ import unittest
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode, LeafNode
 
-from util import text_node_to_html_node
+from util import text_node_to_html_node, split_nodes_delimiter
 
 
 class TestTextNodeToHTMLNode(unittest.TestCase):
@@ -38,6 +38,41 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
         self.assertEqual(html_node.tag, "i")
         self.assertEqual(html_node.value, "This is an italic text node")
 
+class TestSplitNodeDelimiter(unittest.TestCase):
+    def test_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ])
+    def test_non_ext_type_node(self):
+        node = TextNode("print('hi')", TextType.CODE)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [
+            TextNode("print('hi')", TextType.CODE),
+        ])
+    def test_no_closing_delim(self):
+        node = TextNode("This is text with a `code block", TextType.TEXT)
+        self.assertRaises(Exception, split_nodes_delimiter, ([node], "`", TextType.CODE))
+
+    def test_no_delim(self):
+        node = TextNode("This is text with a code block", TextType.TEXT)
+        self.assertRaises(Exception, split_nodes_delimiter, ([node], "`", TextType.CODE))
+    
+    def test_multi_node(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        node2 = TextNode("This is text with another `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node, node2], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+            TextNode("This is text with another ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ])
 
 
 if __name__ == "__main__":
--- src/util.py
+++ src/util.py
@@ -9,3 +9,20 @@ def text_node_to_html_node(text_node):
     else:
         return LeafNode(text_node.text_type.value, text_node.text)
 
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        if delimiter not in old_node.text:
+            raise Exception(f"invalid mardown, delimiter '{delimiter}' not found in text.")
+        old_node_split = old_node.text.split(delimiter)
+        if len(old_node_split) % 2 == 0:
+            raise Exception(f"invalid markdown, no closing delimiter '{delimiter}' found.")
+        for index, section in enumerate(old_node_split):
+            if index % 2 != 0:
+                new_nodes.append(TextNode(section, text_type))
+            else:
+                new_nodes.append(TextNode(section, TextType.TEXT))
+    return new_nodes
